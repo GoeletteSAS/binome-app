8efa683d1afe66bae11e2e4aacddd63d195ba417
Baudouin deBayser <baud2bsr@hotmail.fr>
2024-12-05 10:49:41 +0100

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_140933) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_165245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_140933) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "postname"
+    t.text "firm"
     t.index ["user_id"], name: "index_experiences_on_user_id"
   end
 
@@ -102,6 +104,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_140933) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "timeshare"
     t.index ["experience_id"], name: "index_searches_on_experience_id"
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
@@ -127,6 +130,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_140933) do
     t.string "last_name"
     t.string "username"
     t.date "date_of_birth"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
